9a9dba6aab46dc8bffc50602260c1df6cc3e465b
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-06-08 08:26:20 +0530

    Time: 13 ms (32.75%), Space: 43.8 MB (16.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,49 +1,16 @@
-class Solution {
-    public boolean isPresentAlready(char[][] board,int r , int c){
-        int temp = board[r][c];
-
-        for (int i = 0; i < 9; i++) {
-            if( i!=r && board[i][c] == temp ){
-                return true;
-            }
-        }
-        for (int i = 0; i < 9; i++) {
-            if(i!= c && board[r][i] == temp){
-                return true;
-
-            }
-        }
-
-        int x = (r/3)*3;
-        int y = (c/3)*3;
-
-        for (int i = x; i < x+3; i++) {
-            for (int j = y; j < y+3; j++) {
-
-                if(i==r && j == c){
-                    continue;
-                }
-                if (board[i][j] == temp  ){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+class Solution {   
     public boolean isValidSudoku(char[][] board) {
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-
-                if(board[i][j] != '.'){
-                    if (isPresentAlready(board,i,j)){
-                        return false;
-                    }
-                }
-
-            }
+    Set<String> seen = new HashSet<>();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                    return false;
         }
-        return true;
-
     }
+    return true;
+}
 }
