059b422d2a927918147ca52b80e5f97908394afc
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-06-22 22:58:35 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (23.18%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,17 +1,29 @@
 class Solution {
-        public int searchInsert(int[] nums, int target) {
-        return binaryS(nums, 0, nums.length-1, target);
-    }
-    private int binaryS(int[] nums, int s, int e, int t) {
-        if(s > e)
-            return s;
-        int mid = s + (e-s)/2;
-        if(t == nums[mid])
+        public int binarySearch(int i, int j, int[] nums, int target) {
+        if (i > j) {
+            return i;
+        }
+        int mid = i + (j - i) / 2;
+
+        if (nums[mid] == target) {
             return mid;
-        if(t < nums[mid])
-            e = mid-1;
-        else
-            s = mid+1;
-        return binaryS(nums, s, e, t);
+        }
+        if (target < nums[mid]) {
+           j = mid - 1;
+        } else {
+            i = mid + 1;
+        }
+
+        return binarySearch(i, j, nums, target);
+
+
+    }
+
+    public int searchInsert(int[] nums, int target) {
+
+
+        return binarySearch(0, nums.length - 1, nums, target);
+
+
     }
 }
