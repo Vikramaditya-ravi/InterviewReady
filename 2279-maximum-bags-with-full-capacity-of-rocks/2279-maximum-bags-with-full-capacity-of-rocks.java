36fb42190debccf81b4822b348d40c9f693a79d5
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-08-17 19:46:33 +0530

    Time: 13 ms (98.79%), Space: 54.3 MB (82.73%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,28 +1,17 @@
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-         int n = capacity.length;
-         int res = 0;
-         for (int i = 0; i <n; i++) {
-             if (capacity[i] - rocks[i] == 0){
-                 res++;
-             }
-             else {
-                 pq.offer(capacity[i] - rocks[i]);
-             }
+      int n = capacity.length;
+          int result = 0;
+          for (int i = 0; i < n; i++) {
+              rocks[i] = capacity[i]-rocks[i];
+          }
+          Arrays.sort(rocks);
 
-         }
-         while (additionalRocks > 0 && !pq.isEmpty()){
-             int temp = pq.poll();
-             if (additionalRocks-temp >= 0 ){
-                 additionalRocks = additionalRocks-temp;
-                 res++;
-             }
-             else{
-                 break;
-             }
-             
-         }
-         return res;
+          for (int i = 0; i < n && rocks[i] - additionalRocks <= 0; i++) {
+              result++;
+              additionalRocks = additionalRocks-rocks[i];
+              
+          }
+          return result;
     }
 }
