5615998b9638b697715d7f79f3e41873142b91b8
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-08-19 22:38:08 +0530

    Time: 5 ms (80.13%), Space: 44.6 MB (15.33%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,34 +1,12 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
+      public int islandPerimeter(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int result = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    int count = 4;
-                    if (isValid(i - 1, j, grid.length, grid[0].length)) {
-                        if (grid[i - 1][j] == 1) {
-                            count--;
-                        }
-                    }
-                    if (isValid(i + 1, j, grid.length, grid[0].length)) {
-                        if (grid[i + 1][j] == 1) {
-                            count--;
-                        }
-                    }
-                    if (isValid(i, j - 1, grid.length, grid[0].length)) {
-                        if (grid[i][j - 1] == 1) {
-                            count--;
-                        }
-                    }
-                    if (isValid(i, j + 1, grid.length, grid[0].length)) {
-                        if (grid[i][j + 1] == 1) {
-                            count--;
-                        }
-
-                    }
-                    result+=count;
+                    result += helper(i, j, grid);
                 }
             }
         }
@@ -36,11 +14,21 @@ class Solution {
 
     }
 
-    public boolean isValid(int i, int j, int n, int m) {
-        if (i < 0 || j < 0 || i >= n || j >= m) {
-            return false;
-        } else {
-            return true;
+    public int helper(int i, int j, int[][] grid) {
+        int count = 4;
+        if (i - 1 >= 0 && grid[i - 1][j] == 1  ) {
+            count--;
         }
+        if (i + 1 < grid.length && grid[i + 1][j] == 1 ) {
+            count--;
+        }
+        if (  j - 1 >= 0 && grid[i][j - 1] == 1) {
+            count--;
+        }
+        if ( j+1 < grid[0].length && grid[i][j + 1] == 1 ) {
+            count--;
+        }
+        return count;
+
     }
 }
