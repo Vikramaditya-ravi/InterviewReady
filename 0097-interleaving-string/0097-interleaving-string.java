e0685e848f10d4efff760615ae415b0e71ecd31b
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-08-25 13:14:50 +0530

    Time: 5 ms (48.77%), Space: 40.8 MB (66.43%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,36 +1,35 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         char[] str1 = s1.toCharArray();
-         char[] str2 = s2.toCharArray();
-         char[] str3 = s3.toCharArray();
-        
-      boolean T[][] = new boolean[str1.length +1][str2.length +1];
-        
-        if(str1.length + str2.length != str3.length){
+        char[] str2 = s2.toCharArray();
+        char[] str3 = s3.toCharArray();
+        if (str1.length + str2.length != str3.length) {
             return false;
         }
-        
-        for(int i=0; i < T.length; i++){
-            for(int j=0; j < T[i].length; j++){
-                int l = i + j -1;
-                if(i == 0 && j == 0){
-                    T[i][j] = true;
-                }
-                else if(i == 0){
-                    if(str3[l] == str2[j-1]){
-                        T[i][j] = T[i][j-1];
+        boolean[][] matrix = new boolean[str1.length + 1][str2.length + 1];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int l = i + j - 1;
+
+                if (i == 0 && j == 0) {
+                    matrix[i][j] = true;
+                } else if (i == 0) {
+                    if (str2[j - 1] == str3[l]) {
+                        matrix[i][j] = matrix[i][j - 1];
                     }
-                }
-                else if(j == 0){
-                    if(str1[i-1] == str3[l]){
-                        T[i][j] = T[i-1][j];
+
+                } else if (j == 0) {
+                    if (str1[i - 1] == str3[l]) {
+                        matrix[i][j] = matrix[i - 1][j];
                     }
-                }
-                else{
-                    T[i][j] = (str1[i-1] == str3[l] ? T[i-1][j] : false) || (str2[j-1] == str3[l] ? T[i][j-1] : false);
+                } else {
+
+                    matrix[i][j] = (str1[i - 1] == str3[l] && matrix[i - 1][j]) ||
+                           (str2[j - 1] == str3[l] && matrix[i][j - 1]);
                 }
             }
         }
-        return T[str1.length][str2.length];
+        return matrix[str1.length][str2.length];
     }
 }
