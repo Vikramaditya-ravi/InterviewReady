b2be56792f20edc659185e1db5611d1045052ebf
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-09-30 15:56:39 +0530

    Time: 6 ms (14.92%), Space: 54.3 MB (47.97%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div643-maximum-average-subarray-i/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div643-maximum-average-subarray-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+           int n = nums.length;
+
+    double result = Integer.MIN_VALUE;;
+// avg;
+       double sum =0;
+               for (int i = 0; i < n; i++) {
+
+        if( i < k){
+        sum+=nums[i];
+        }
+        else {
+        sum = sum+nums[i] - nums[i-k];
+        }
+       if(i>=k-1) {
+           double avg = sum/k;
+        result = Math.max(result,avg);
+       }
+               }
+
+        
+        return result;
+
+    }
+}
