efedad8d3c5414f83e7f19e94ae81e065f2165c8
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-10-09 16:21:15 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (38.52%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int l = 0;
+      int l = 0;
         int r = nums.length - 1;
         int ans = -1;
         int[] result = new int[2];
@@ -11,8 +11,26 @@ class Solution {
 
             if (target == nums[mid]) {
                 //return the answer.
-                ans = mid;
-                r = mid - 1;
+                l = mid;
+                r = mid;
+                while (l >= 0) {
+                    if ( l-1 > -1 && target == nums[l - 1]) {
+                        l--;
+                    } else {
+                        break;
+                    }
+                }
+                while (r < nums.length) {
+                    if (r+1 < nums.length &&  target == nums[r + 1]) {
+                        r++;
+                    } else {
+                        break;
+                    }
+                }
+                result[0] = l;
+                result[1] = r;
+                return result;
+
 
             } else if (target > nums[mid]) {
                 l = mid + 1;
@@ -20,26 +38,6 @@ class Solution {
                 r = mid - 1;
             }
         }
-        result[0] = ans;
-        l = 0;
-        r = nums.length - 1;
-
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            if (target == nums[mid]) {
-                //return the answer.
-                ans = mid;
-                l = mid + 1;
-
-            } else if (target > nums[mid]) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
-            }
-        }
-        result[1] = ans;
 
         return result;
     }
