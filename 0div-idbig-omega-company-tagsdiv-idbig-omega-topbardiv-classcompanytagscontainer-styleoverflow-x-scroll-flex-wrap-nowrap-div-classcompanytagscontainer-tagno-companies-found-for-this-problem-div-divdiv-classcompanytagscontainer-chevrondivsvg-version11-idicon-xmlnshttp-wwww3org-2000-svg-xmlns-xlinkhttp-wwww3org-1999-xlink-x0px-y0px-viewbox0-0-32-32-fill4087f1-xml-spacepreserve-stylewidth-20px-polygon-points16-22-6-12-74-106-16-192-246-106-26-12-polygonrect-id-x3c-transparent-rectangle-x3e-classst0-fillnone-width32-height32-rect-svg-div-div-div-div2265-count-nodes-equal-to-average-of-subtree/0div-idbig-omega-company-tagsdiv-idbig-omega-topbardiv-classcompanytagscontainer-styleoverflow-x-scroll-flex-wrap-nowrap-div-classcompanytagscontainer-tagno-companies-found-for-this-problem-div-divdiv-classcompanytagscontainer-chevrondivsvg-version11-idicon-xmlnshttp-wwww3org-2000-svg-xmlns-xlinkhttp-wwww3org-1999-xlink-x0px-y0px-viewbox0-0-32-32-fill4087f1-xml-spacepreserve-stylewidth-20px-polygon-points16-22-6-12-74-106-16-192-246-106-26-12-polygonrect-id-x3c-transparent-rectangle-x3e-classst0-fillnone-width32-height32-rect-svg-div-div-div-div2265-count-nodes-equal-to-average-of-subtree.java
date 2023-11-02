e5f634a095e804c12d5b6cef9926795bc98eac8d
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-02 15:42:34 +0530

    Time: 1 ms (48.09%), Space: 43 MB (55.43%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree.java
@@ -1,67 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
        public int count;
-    public int preOrder(TreeNode root, HashMap<TreeNode, int[]> map) {
-
+       public int[] preOrderArray(TreeNode root) {
+        int[] nums = new int[2];
         if (root.left == null && root.right == null) {
-            map.put(root, new int[]{1, root.val});
+            nums[0] = root.val;
+            nums[1] = 1;
         }
-        if (root.left != null){            
-                preOrder(root.left,map);            
+
+
+        if (root.left != null && root.right != null) {
+            int[] left = preOrderArray(root.left);
+            int[] right = preOrderArray(root.right);
+            nums[0] = left[0] + right[0] + root.val;
+            nums[1] = left[1] + right[1] + 1;
+
+        } else if (root.left != null) {
+            int[] left = preOrderArray(root.left);
+            nums[0] = left[0] + root.val;
+            nums[1] = left[1] + 1;
+
+        } else if (root.right != null) {
+            int[] right = preOrderArray(root.right);
+            nums[0] = right[0] + root.val;
+            nums[1] = right[1] + 1;
         }
-        if (root.right != null){
-            preOrder(root.right,map);            
-        }      
-        
-        if (map.containsKey(root.left) && map.containsKey(root.right)){
-            map.put(root, new int[]{
-                        1 + map.get(root.left)[0] + map.get(root.right)[0],
-                        root.val + map.get(root.left)[1] + map.get(root.right)[1]
-                });            
-        }else           
-        if (map.containsKey(root.left)){
-            map.put(root, new int[]{1 + map.get(root.left)[0], map.get(root.left)[1] + root.val});
-            
-        }else 
-        if (map.containsKey(root.right)){
-            map.put(root, new int[]{1 + map.get(root.right)[0], map.get(root.right)[1] + root.val});
-            
-        }         
-        if (map.get(root)[1] / map.get(root)[0] == root.val) {
+        if (nums[0] / nums[1] == root.val) {
             count++;
         }
-        return count;       
-       
-
-
-        
-//        root.val = root.val + preOrder(root.left) + preOrder(root.right);
-//        int size = preOrder2(root);
-//        if (sum / size == root.val) {
-//            count++;
-//        }
-//        return sum;
+        return nums;
 
     }
 
     public int averageOfSubtree(TreeNode root) {
-        HashMap<TreeNode, int[]> map = new HashMap<>();
         count = 0;
-        preOrder(root, map);
+        preOrderArray(root);
         return count;
     }
 }
