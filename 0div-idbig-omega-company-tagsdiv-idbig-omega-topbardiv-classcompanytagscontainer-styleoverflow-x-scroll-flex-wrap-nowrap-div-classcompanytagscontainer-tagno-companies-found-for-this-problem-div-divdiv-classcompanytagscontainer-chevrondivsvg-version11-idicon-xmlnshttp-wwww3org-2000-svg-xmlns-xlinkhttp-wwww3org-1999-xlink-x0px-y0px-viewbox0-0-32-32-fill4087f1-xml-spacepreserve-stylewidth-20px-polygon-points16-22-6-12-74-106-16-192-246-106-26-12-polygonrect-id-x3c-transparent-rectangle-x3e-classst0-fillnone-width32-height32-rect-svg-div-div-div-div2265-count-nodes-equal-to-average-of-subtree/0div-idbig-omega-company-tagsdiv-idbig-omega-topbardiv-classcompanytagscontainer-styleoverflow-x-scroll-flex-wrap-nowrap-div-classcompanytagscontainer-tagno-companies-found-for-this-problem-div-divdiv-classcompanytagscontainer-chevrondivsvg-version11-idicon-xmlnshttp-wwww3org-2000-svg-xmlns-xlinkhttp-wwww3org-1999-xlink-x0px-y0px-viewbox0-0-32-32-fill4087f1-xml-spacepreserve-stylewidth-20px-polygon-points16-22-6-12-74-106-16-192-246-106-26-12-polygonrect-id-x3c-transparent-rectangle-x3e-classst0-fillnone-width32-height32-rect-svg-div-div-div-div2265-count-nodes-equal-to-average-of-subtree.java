22c51e7da8b7c9f3839320af01c4b59f726f7c12
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-02 17:11:43 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (36.07%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2265-count-nodes-equal-to-average-of-subtree.java
@@ -1,30 +1,19 @@
 class Solution {
        public int count;
        public int[] preOrderArray(TreeNode root) {
+          
+    if (root == null) {
+            return new int[]{0, 0};
+        }
+        
         int[] nums = new int[2];
-        if (root.left == null && root.right == null) {
-            nums[0] = root.val;
-            nums[1] = 1;
-        }
+        int[] left = preOrderArray(root.left);
+        int[] right = preOrderArray(root.right);
+        
+        nums[0] = left[0] + right[0] + root.val;
+        nums[1] = left[1] + right[1] + 1;
 
-
-        if (root.left != null && root.right != null) {
-            int[] left = preOrderArray(root.left);
-            int[] right = preOrderArray(root.right);
-            nums[0] = left[0] + right[0] + root.val;
-            nums[1] = left[1] + right[1] + 1;
-
-        } else if (root.left != null) {
-            int[] left = preOrderArray(root.left);
-            nums[0] = left[0] + root.val;
-            nums[1] = left[1] + 1;
-
-        } else if (root.right != null) {
-            int[] right = preOrderArray(root.right);
-            nums[0] = right[0] + root.val;
-            nums[1] = right[1] + 1;
-        }
-        if (nums[0] / nums[1] == root.val) {
+        if (nums[1] != 0 && nums[0] / nums[1] == root.val) {
             count++;
         }
         return nums;
