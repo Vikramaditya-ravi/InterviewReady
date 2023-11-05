981d94c0d95a09c0abc6d8c8eea1e8a7986b3dff
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-05 16:04:28 +0530

    Time: 47 ms (5.29%), Space: 56.7 MB (24.60%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div714-best-time-to-buy-and-sell-stock-with-transaction-fee/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+       int n = prices.length;
+        List<List<Integer>> dp = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.addAll(Arrays.asList(-1, -1));
+            dp.add(row);
+        }
+
+        dp.get(n).add(0, 0);
+        dp.get(n).add(1, 0);
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 1) {
+                    profit = Math.max(-prices[ind] + dp.get(ind + 1).get(0),
+                            dp.get(ind + 1).get(1));
+                } else {
+                    profit = Math.max(prices[ind] + dp.get(ind + 1).get(1)-fee,
+                            dp.get(ind + 1).get(0));
+                }
+                dp.get(ind).add(buy, profit);
+            }
+
+        }
+        return dp.get(0).get(1); 
+    }
+}
