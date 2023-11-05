3e012f26578552243884a3b27c5da1a7bffa5c72
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-05 15:57:15 +0530

    Time: 2 ms (19.21%), Space: 42.9 MB (11.27%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,36 +1,33 @@
 class Solution {
-    public int utility(int ind, int buy, int[] prices, ArrayList<ArrayList<Integer>> list) {
-        if (ind >= prices.length) {
-            return 0;
-        }
-        if (list.get(ind).get(buy) != -1) {
-            return list.get(ind).get(buy);
-        }
-        int profit = 0;
-        if (buy == 1) {
-            profit = Math.max(-prices[ind] + utility(ind + 1, 0, prices, list),
-                    utility(ind + 1, 1, prices, list));
-
-        } else {
-            profit = Math.max(prices[ind] + utility(ind + 2, 1, prices, list),
-                    utility(ind + 1, 0, prices, list));
-
-        }
-        list.get(ind).set(buy, profit);
-        return profit; 
-
-    }
-
     public int maxProfit(int[] prices) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+       ArrayList<ArrayList<Integer>> list = new ArrayList<>();
         int n = prices.length;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n + 2; i++) {
             ArrayList<Integer> row = new ArrayList<>();
-            row.addAll(Arrays.asList(-1, -1));
+            row.addAll(Arrays.asList(0, 0));
             list.add(row);
         }
+        list.get(n).add(0, 0);
+        list.get(n).add(1, 0);
 
-        return utility(0, 1, prices, list);
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 1) {
+                    profit = Math.max(-prices[ind] + list.get(ind + 1).get(0),
+                            list.get(ind + 1).get(1));
+
+                } else {
+                    profit = Math.max(prices[ind] + list.get(ind + 2).get(1),
+                            list.get(ind + 1).get(0));
+
+
+                }
+                list.get(ind).set(buy, profit);
+            }
+        }
+        return list.get(0).get(1);
 
 
     }
