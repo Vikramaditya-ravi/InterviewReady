e0a7dff4caeab977270528d664066748ec89d2c6
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-06 23:19:40 +0530

    Time: 72 ms (59.77%), Space: 41.2 MB (71.31%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div312-burst-balloons/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div312-burst-balloons.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div312-burst-balloons/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div312-burst-balloons.java
@@ -1,39 +1,70 @@
 class Solution {
-     public int utility(int i, int j, ArrayList<Integer> list, int[][] dp) {
-        if (i > j) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int maxi = Integer.MIN_VALUE;
-        for (int ind = i; ind <= j; ind++) {
-            int cost = list.get(i - 1) * list.get(ind) * list.get(j + 1)
-                    + utility(i, ind - 1, list, dp)
-                    + utility(ind + 1, j, list, dp);
-            maxi = Math.max(cost, maxi);
-        }
+//      public int utility(int i, int j, ArrayList<Integer> list, int[][] dp) {
+//         if (i > j) {
+//             return 0;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         int maxi = Integer.MIN_VALUE;
+//         for (int ind = i; ind <= j; ind++) {
+//             int cost = list.get(i - 1) * list.get(ind) * list.get(j + 1)
+//                     + utility(i, ind - 1, list, dp)
+//                     + utility(ind + 1, j, list, dp);
+//             maxi = Math.max(cost, maxi);
+//         }
 
-        return dp[i][j] = maxi;
-    }
+//         return dp[i][j] = maxi;
+//     }
 
-    public int maxCoins(int[] nums) {
+//     public int maxCoins(int[] nums) {
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         list.add(1);
+//         int n = nums.length;
+//         for (int num : nums) {
+//             list.add(num);
+//         }
+//         list.add(1);
+
+//         int[][] dp = new int[n + 1][n + 1];
+//          for (int[] ints : dp) {
+//             Arrays.fill(ints, -1);
+//         }
+
+//         return utility(1, n, list, dp);
+
+
+//     }
+     public int maxCoins(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
-
         list.add(1);
         int n = nums.length;
         for (int num : nums) {
             list.add(num);
         }
         list.add(1);
-
-        int[][] dp = new int[n + 1][n + 1];
-         for (int[] ints : dp) {
-            Arrays.fill(ints, -1);
+        int[][] dp = new int[n + 2][n + 2];
+        for (int[] ints : dp) {
+            Arrays.fill(ints, 0);
         }
 
-        return utility(1, n, list, dp);
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n; j++) {
 
-
+                if (i > j) {
+                    continue;
+                }
+                int maxi = Integer.MIN_VALUE;
+                for (int ind = i; ind <= j; ind++) {
+                    int cost = list.get(i - 1) * list.get(ind) * list.get(j + 1)
+                            + dp[i][ind - 1]
+                            + dp[ind + 1][j];
+                    maxi = Math.max(cost, maxi);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     }
 }
