f5a32e9e5472ec204893fafc79147f19d687d110
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-07 15:18:43 +0530

    Time: 16 ms (100.00%), Space: 55.3 MB (54.17%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
@@ -1,11 +1,19 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
-       for (int i = 0; i < dist.length; ++i)
-        dist[i] = (dist[i] - 1) / speed[i];
-    Arrays.sort(dist);
-    for (int i = 0; i < dist.length; ++i)
-        if (i > dist[i])
-            return i;
-    return dist.length;
+        int n = dist.length;
+        int[] time = new int[n];
+        for(int i =0 ; i < n ; i++){
+            time[i] = (dist[i]-1)/speed[i];
+        }
+        Arrays.sort(time);
+        for(int i = 0;i < n; i++){
+            if(i > time[i]){
+                return i;
+            }
+        }
+        return dist.length;
+       
+
+        
     }
 }
