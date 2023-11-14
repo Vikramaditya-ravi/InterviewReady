df979d0ada18c1c35135684cd39ce232cd64d7ee
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-14 19:27:00 +0530

    Time: 17 ms (97.79%), Space: 44.5 MB (49.02%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1930-unique-length-3-palindromic-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1930-unique-length-3-palindromic-subsequences.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1930-unique-length-3-palindromic-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1930-unique-length-3-palindromic-subsequences.java
@@ -1,52 +1,39 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
          int n = s.length();
-
-        char[] chArr = s.toCharArray();
-
-        int[] firstOcc = new int[26];
-        int[] lastOcc = new int[26];
-
-        Arrays.fill(firstOcc, -1);
-        Arrays.fill(lastOcc, -1);
+        int[] first = new int[26];
+        int[] last = new int[26];
+        
+        char[] charArray = s.toCharArray();
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
 
         for (int i = 0; i < n; i++) {
-
-            char ch = chArr[i];
-
-            if (firstOcc[ch - 'a'] == -1) {
-                firstOcc[ch - 'a'] = i;
+            if (first[charArray[i] - 'a'] == -1) {
+                first[charArray[i] - 'a'] = i;
             }
-
-            lastOcc[ch - 'a'] = i;
+            last[charArray[i] - 'a'] = i;
         }
-
-        int ans = 0, count = 0;
-
+        
+        int ans = 0,count = 0;
         boolean[] visited;
+        
 
-        // check for each character ( start or end of palindrome )
         for (int i = 0; i < 26; i++) {
-
-            int si = firstOcc[i]; // si - starting index
-            int ei = lastOcc[i]; // ei - ending index
-
-            visited = new boolean[26];
-
+            visited= new boolean[26];
+            int start = first[i];
+            int end = last[i];          
             count = 0;
 
-            // check for unique charcters ( middle of palindrome )
-            for (int j = si + 1; j < ei; j++) {
-
-                if (!visited[chArr[j] - 'a']) {
-                    visited[chArr[j] - 'a'] = true;
+            for (int j = start + 1; j < end; j++) {
+                if (!visited[charArray[j] - 'a']) {
+                    visited[charArray[j] - 'a'] = true;
                     count++;
                 }
             }
-
             ans += count;
-        }
 
+        }
         return ans;
     }
 }
