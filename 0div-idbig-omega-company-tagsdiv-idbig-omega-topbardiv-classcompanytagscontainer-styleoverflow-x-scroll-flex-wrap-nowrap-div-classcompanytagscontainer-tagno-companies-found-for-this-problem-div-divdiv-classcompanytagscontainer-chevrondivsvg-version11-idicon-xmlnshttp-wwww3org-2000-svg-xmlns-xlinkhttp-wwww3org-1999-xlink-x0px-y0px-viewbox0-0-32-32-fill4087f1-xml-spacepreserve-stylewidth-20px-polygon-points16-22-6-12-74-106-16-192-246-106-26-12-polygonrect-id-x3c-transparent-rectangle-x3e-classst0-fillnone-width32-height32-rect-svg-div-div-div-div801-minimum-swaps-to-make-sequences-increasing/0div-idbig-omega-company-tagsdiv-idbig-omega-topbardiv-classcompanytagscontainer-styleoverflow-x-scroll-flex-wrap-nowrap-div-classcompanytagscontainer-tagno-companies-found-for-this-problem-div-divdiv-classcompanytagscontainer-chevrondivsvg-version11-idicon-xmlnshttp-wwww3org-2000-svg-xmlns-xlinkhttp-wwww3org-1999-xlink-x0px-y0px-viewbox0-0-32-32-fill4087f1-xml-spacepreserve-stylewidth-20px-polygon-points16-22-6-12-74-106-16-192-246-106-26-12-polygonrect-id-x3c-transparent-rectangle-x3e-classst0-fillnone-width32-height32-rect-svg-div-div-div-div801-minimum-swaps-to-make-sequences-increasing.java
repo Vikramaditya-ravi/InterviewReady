68fc1251b8c7cc44927db2859e8739296799b309
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-17 21:10:44 +0530

    Time: 33 ms (30.35%), Space: 59.2 MB (50.75%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div801-minimum-swaps-to-make-sequences-increasing/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div801-minimum-swaps-to-make-sequences-increasing.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div801-minimum-swaps-to-make-sequences-increasing/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div801-minimum-swaps-to-make-sequences-increasing.java
@@ -1,52 +1,47 @@
 class Solution {
-   int solve(ArrayList<Integer> list1, ArrayList<Integer> list2, int index, int swapped, int[][] dp) {
-        if (index >= list1.size()) {
-            return 0;
-        }
-
-        if (dp[index][swapped] != -1) {
-            return dp[index][swapped];
-        }
-
-        int prev1 = list1.get(index - 1);
-        int prev2 = list2.get(index - 1);
-
-        if (swapped == 1) {
-            int temp = prev2;
-            prev2 = prev1;
-            prev1 = temp;
-        }
-        int ans = Integer.MAX_VALUE;
-
-        //noSwap
-
-        if (list1.get(index) > prev1 && list2.get(index) > prev2) {
-            ans = solve(list1, list2, index + 1, 0, dp);
-        } 
-       
-       if(list1.get(index) > prev2 && list2.get(index) > prev1){
-            ans = Math.min(ans, 1 + solve(list1, list2, index + 1, 1, dp));
-            
-        }
-        return dp[index][swapped] = ans;
-    }
-
     public int minSwap(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list1 = new ArrayList<>();
+         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
-
-        int[][] dp = new int[nums1.length+1][2];
-        for (int[] num : dp) {
-            Arrays.fill(num, -1);
-        }
+        int n = nums1.length;
 
         list1.add(-1);
         list2.add(-1);
-
-        for (int i = 0; i < nums1.length; i++) {
+        
+        for (int i = 0; i < n; i++) {
             list1.add(nums1[i]);
             list2.add(nums2[i]);
         }
-        return solve(list1, list2, 1, 0, dp);
+        n= list1.size();
+        int[][] dp = new int[n + 1][2];
+
+
+
+        for (int index = n - 1; index >= 1; index--) {
+            for (int swapped = 1; swapped >= 0; swapped--) {
+
+                int prev1 = list1.get(index - 1);
+                int prev2 = list2.get(index - 1);
+
+                if (swapped == 1) {
+                    int temp = prev2;
+                    prev2 = prev1;
+                    prev1 = temp;
+                }
+                int ans = Integer.MAX_VALUE;
+                //noSwap
+
+                if (list1.get(index) > prev1 && list2.get(index) > prev2) {
+                    ans = dp[index + 1][0];
+                }
+                //swapped
+
+                if (list1.get(index) > prev2 && list2.get(index) > prev1) {
+                    ans = Math.min(ans, 1 + dp[index + 1][1]);
+                }
+                dp[index][swapped] = ans;
+
+            }
+        }
+        return dp[1][0];
     }
 }
