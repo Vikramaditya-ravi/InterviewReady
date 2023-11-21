b841dfa78f8bf9b66f14533835872712306d30f3
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2023-11-21 18:11:31 +0530

    Time: 78 ms (19.21%), Space: 94.8 MB (13.28%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div300-longest-increasing-subsequence.java
@@ -3,32 +3,24 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int[][] dp = new int[n][n + 1];
+        int[][] dp = new int[n + 1][n + 1];
 
-        for (int[] temp : dp) {
-            Arrays.fill(temp, -1);
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+                //take
+                int take = 0, noTake;
+
+                if (prev == -1 || nums[curr] > nums[prev]) {
+                    take = 1 + dp[curr + 1][curr + 1];
+                }
+                //
+                noTake = dp[curr + 1][prev + 1];
+                dp[curr][prev + 1] = Math.max(take, noTake);
+            }
+
         }
-        return solve(-1, 0, nums, dp);
+
+        return dp[0][0];
     }
-     public int solve(int prev, int curr, int[] nums, int[][] dp) {
-        if (curr >= nums.length) {
-            return 0;
-        }
-
-        if (dp[curr][prev + 1] != -1) {
-            return dp[curr][prev + 1];
-        }
-
-        //take
-        int take = 0, noTake;
-
-        if (prev == -1 || nums[curr] > nums[prev]) {
-            take = 1 + solve(curr, curr + 1, nums, dp);
-        }
-        //
-        noTake = solve(prev, curr + 1, nums, dp);
-        return dp[curr][prev + 1] = Math.max(take, noTake);
-
-    }
-
+    
 }
