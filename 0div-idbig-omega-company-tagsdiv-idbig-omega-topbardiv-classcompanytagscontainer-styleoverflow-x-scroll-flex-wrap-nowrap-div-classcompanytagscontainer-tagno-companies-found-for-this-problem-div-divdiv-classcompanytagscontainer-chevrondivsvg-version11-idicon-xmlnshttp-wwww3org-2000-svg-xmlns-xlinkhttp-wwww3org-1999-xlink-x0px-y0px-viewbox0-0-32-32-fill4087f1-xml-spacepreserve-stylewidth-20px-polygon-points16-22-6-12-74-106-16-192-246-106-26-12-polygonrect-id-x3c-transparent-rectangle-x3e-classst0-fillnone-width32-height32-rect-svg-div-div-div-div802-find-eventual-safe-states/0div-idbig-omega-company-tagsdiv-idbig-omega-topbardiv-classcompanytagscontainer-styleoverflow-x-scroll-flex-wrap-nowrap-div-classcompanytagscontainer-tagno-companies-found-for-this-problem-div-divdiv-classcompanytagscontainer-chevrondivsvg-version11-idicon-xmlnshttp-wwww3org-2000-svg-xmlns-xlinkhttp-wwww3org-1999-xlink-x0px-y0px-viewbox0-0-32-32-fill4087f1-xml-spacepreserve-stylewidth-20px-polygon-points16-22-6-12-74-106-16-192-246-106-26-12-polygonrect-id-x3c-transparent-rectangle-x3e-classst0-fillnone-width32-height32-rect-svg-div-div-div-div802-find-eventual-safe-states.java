a426a563464758012612fe6831931e57e41b039c
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2024-04-17 17:20:38 +0530

    Time: 363 ms (5.04%), Space: 55.2 MB (67.06%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div802-find-eventual-safe-states/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div802-find-eventual-safe-states.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div802-find-eventual-safe-states/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div802-find-eventual-safe-states.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean dfsUtility(ArrayList<ArrayList<Integer>> list,int[] vis,int current){
-        
+    public boolean dfsUtility(int[][] graph,int[] vis,int current){        
         vis[current] = 2;
-        for(int val:list.get(current)){
+        
+        for(int val:graph[current]){
             if(vis[val] == 2){
                 return true;
             }
-            else if(vis[val] == 0 && dfsUtility(list,vis,val) ){
+            else if(vis[val] == 0 && dfsUtility(graph,vis,val) ){
                 return true;
             }
            
@@ -17,29 +17,17 @@ class Solution {
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> result = new ArrayList<>();
-        int n =graph.length;        
-        ArrayList<ArrayList<Integer>> list  = new ArrayList<>();
-
-        for(int i = 0 ; i <= n;i++){
-            list.add(new ArrayList<>());
-        }
-        
-        for(int i = 0; i < n; i++){
-            for(int  j =0; j < graph[i].length; j++){
-                list.get(i).add(graph[i][j]);
-            }
-        }        
+        int n =graph.length;
+             
         
         for(int i= 0; i < n; i++ ){
             int[] vis = new int[n];
-                if(!dfsUtility(list,vis,i)){
+                if(!dfsUtility(graph,vis,i)){
                     result.add(i);
-                }
-            
+                }           
         }
 
-        return result;
-        
+        return result;        
         
         
     }
