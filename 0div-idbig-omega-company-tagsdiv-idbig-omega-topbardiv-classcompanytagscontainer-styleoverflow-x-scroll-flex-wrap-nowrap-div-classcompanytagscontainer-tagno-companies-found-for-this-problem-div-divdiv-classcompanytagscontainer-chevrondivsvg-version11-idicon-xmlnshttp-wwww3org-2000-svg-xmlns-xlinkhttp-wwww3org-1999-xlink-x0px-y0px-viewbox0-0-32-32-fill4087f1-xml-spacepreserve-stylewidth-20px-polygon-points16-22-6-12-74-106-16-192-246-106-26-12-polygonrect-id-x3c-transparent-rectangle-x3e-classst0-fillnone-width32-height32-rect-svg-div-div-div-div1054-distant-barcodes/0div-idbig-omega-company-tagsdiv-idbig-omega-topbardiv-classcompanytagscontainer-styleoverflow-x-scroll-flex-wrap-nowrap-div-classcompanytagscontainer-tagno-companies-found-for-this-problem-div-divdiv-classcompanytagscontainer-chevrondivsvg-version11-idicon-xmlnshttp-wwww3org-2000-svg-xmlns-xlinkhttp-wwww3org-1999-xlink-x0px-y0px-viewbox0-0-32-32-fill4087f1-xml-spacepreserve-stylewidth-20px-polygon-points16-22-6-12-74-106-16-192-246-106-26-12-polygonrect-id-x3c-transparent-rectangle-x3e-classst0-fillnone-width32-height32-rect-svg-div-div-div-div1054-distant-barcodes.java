770ce106dc938f5969cf89e7a2ba86d1b64e4696
Ravi Prakash <57453761+Vikramaditya-ravi@users.noreply.github.com>
2024-06-26 16:23:02 +0530

    Time: 31 ms (47.44%), Space: 45.9 MB (76.07%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1054-distant-barcodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1054-distant-barcodes.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1054-distant-barcodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1054-distant-barcodes.java
@@ -1,21 +1,29 @@
 class Solution {
     public int[] rearrangeBarcodes(int[] barcodes) {
-        Map<Integer, Integer> cnt = new HashMap();
-        for (int i : barcodes) cnt.put(i, cnt.getOrDefault(i, 0) + 1);
-
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(cnt.entrySet());
+        int n = barcodes.length;
+        int[] result = new int[n];
+        int i =0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int val : barcodes){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
         Collections.sort(list, Map.Entry.<Integer, Integer>comparingByValue().reversed());
-        int l = barcodes.length, i = 0;
-        int[] res = new int[l];
-        for (Map.Entry<Integer, Integer> e : list) {
-            int time = e.getValue();
-            while (time-- > 0) {
-                res[i] = e.getKey();
-                i += 2;
-                if (i >= barcodes.length) i = 1;
+        
+        for(Map.Entry<Integer,Integer> e :list){
+            int temp = e.getValue();
+            while(temp-->0){
+                result[i] = e.getKey();
+                i+=2;
+                if(i>=n){
+                    i =1;
+                }
             }
         }
-        return res;
+        return result;
+        
+        
+        
         
         
         
